87c460518f7dbe175272bf8f7a4bea0e869fbe14
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-07-04 13:11:23 +0300

    new

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', async event =>{
     if(city){
         try{
             const weatherData = await getWeatherData(city);
-            displayWeatherInfo(weatherData)
+            displayWeatherInfo(weatherData);
         }
         catch(error){
             console.error(error);
@@ -23,10 +23,21 @@ weatherForm.addEventListener('submit', async event =>{
 });
 
 async function getWeatherData(city){
-    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    
+    const response = await fetch(apiUrl);
+    if(!response.ok){
+        throw new Error('Could not fetch weather data')
+    } 
+    return await response.json();
 }
 function displayWeatherInfo(data){
-
+    console.log(data);
+    const { name: city,
+            main: {temp, humidity},
+            weather: [{description, id}]} = data;
+    card.textContent = ''
 }
 
 function getWeatherEmoji(weatherId){
